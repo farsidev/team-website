692f4026daafdf3603632e2175c92d329a4a8d98
Mohamadreza <mohamad.r.sadeghi93@gmail.com>
2019-11-14 15:50:58 +0330

    remove next/image from config

--- next.config.js
+++ next.config.js
@@ -1,28 +1,25 @@
 const withSass = require('@zeit/next-sass');
-const withImages = require('next-images');
 
-module.exports = withImages(
-   withSass({
-      cssModules: true,
-      // cssLoaderOptions: {
-      //     importLoaders: 1,
-      //     localIdentName: "[local]___[hash:base64:5]",
-      // },
-      webpack: function(config) {
-         config.module.rules.push({
-            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-            use: {
-               loader: 'url-loader',
-               options: {
-                  limit: 100000,
-                  name: '[name].[ext]',
-               },
+module.exports = withSass({
+   cssModules: true,
+   // cssLoaderOptions: {
+   //     importLoaders: 1,
+   //     localIdentName: "[local]___[hash:base64:5]",
+   // },
+   webpack: function(config) {
+      config.module.rules.push({
+         test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+         use: {
+            loader: 'url-loader',
+            options: {
+               limit: 100000,
+               name: '[name].[ext]',
             },
-         });
-         return config;
-      },
-      sassLoaderOptions: {
-         includePaths: ['absolute/path/a', 'absolute/path/b'],
-      },
-   }),
-);
+         },
+      });
+      return config;
+   },
+   sassLoaderOptions: {
+      includePaths: ['absolute/path/a', 'absolute/path/b'],
+   },
+});
